20dc0a1adbc066d6f2e71c034ce14403ecf0beb9
piesunjohn <piesunjohn@gmail.com>
2018-04-29 01:25:40 +0000

    prepared sample.py for merge with test-branch

--- sample.py
+++ sample.py
@@ -5,10 +5,8 @@ print(my_string)
 
 #this is another edit to confirm that it was a success. 
 
+#this is an edit to test merging between test-branch2 and test-branch. This was written on test-branch
+
+#this is an edit to test merging between test-branch2 and test-branch. This was written on test-branch
 
 
-
-
-
-
-#this is an edit to test merging between test-branch2 and test-branch. This was written on test-branch2
